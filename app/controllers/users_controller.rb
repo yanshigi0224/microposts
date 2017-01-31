f2760120ef9a515bce61a48da6c0c9509d529d07
Shinya Katagi <yanshigi.2.24@gmail.com>
2017-01-31 17:59:00 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user=User.find(params[:id])
   end
@@ -16,10 +18,37 @@ class UsersController < ApplicationController
       render"new"
     end
   end
+
+#   http://servername/user/1/edit
+  def edit
+    @user = User.find(params[:id])
+    # @userは編集対象ユーザー
+    if (current_user != @user)
+      redirect_to root_path
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+     # @userは編集対象ユーザー
+    if (current_user != @user)
+      redirect_to root_path
+    end   
+    
+    if (@user.update(user_profile))
+      redirect_to @user
+    else
+      flash.now[:danger] = "更新できませんでした"
+      render :edit
+    end
+  end
   
   private
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
-  
+
+  def user_profile
+    params.require(:user).permit(:name,:email,:password,:password_confirmation,:location)
+  end  
 end
